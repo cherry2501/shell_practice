30e5e7683bfb4fc80e819905671fb97b8e0c85d2
cherry2501 <saicharangudiya@gmail.com>
2024-10-20 23:19:49 +0530

    installation

--- 11-installation.sh
+++ 11-installation.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 USER_ID=$(id -u)
-if ( $USER_ID -ne 0)
+if [ $USER_ID -ne 0 ]
 then
     echo "Better use root user for installation tasks..."
+    exit 1
 fi
 
 dnf list installed git
