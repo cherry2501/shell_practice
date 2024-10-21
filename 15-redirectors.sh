8a445cc59e191f7784cbd9bbb8ad882697a86174
cherry2501 <saicharangudiya@gmail.com>
2024-10-21 12:37:48 +0530

    redirectors

--- 15-redirectors.sh
+++ 15-redirectors.sh
@@ -3,7 +3,7 @@
 LOG_FOLDER="/var/log/shell-script"
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME/$TIME_STAMP"
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME/$TIME_STAMP.log"
 
 mkdir -p $LOG_FILE
 
