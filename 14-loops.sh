09017fe577b048e2b71bdb22d113f2ab9f1985e3
cherry2501 <saicharangudiya@gmail.com>
2024-10-21 09:25:38 +0530

    loops

--- 14-loops.sh
+++ 14-loops.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-USERID=(id -u)
+USERID=$(id -u)
 if [ $? -ne 0]
 then
     echo "Root user is required to run this script..."
+    exit 1
 fi
 
 VALIDATE(){
     if [ $1 -ne 0]
     then 
-        echo "$2 is not installed..."
+        echo "$2 is not installed... check your script"
+        exit 1
+    else
+        echo "$2 installed successfully..."
     fi
 }
 
 for package in $@
 do 
     dnf list installed $package
-    VALIDATE $? $package
+    if [ $? -ne 0 ]
+    then 
+        echo "$package not installed..."
+        dnf install $package -y
+        VALIDATE $? "Installing $package"
+    else
+        echo "$package is already installed..."
 done
 
