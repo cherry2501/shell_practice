4c81ef46f6d39425c9ee5ab27b51cc940ccf942c
cherry2501 <saicharangudiya@gmail.com>
2024-10-21 09:18:09 +0530

    loops

--- 14-loops.sh
+++ 14-loops.sh
@@ -6,15 +6,16 @@ then
     echo "Root user is required to run this script..."
 fi
 
-for $package in $@
-do 
-    dnf list installed $package
-    VALIDATE $? $package
-done
-
 VALIDATE(){
     if [ $1 -ne 0]
     then 
         echo "$2 is not installed..."
     fi
 }
+
+for package in $@
+do 
+    dnf list installed $package
+    VALIDATE $? $package
+done
+
