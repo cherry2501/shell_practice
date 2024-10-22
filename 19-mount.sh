6167a262df239d590d39eca2dc3b321ea9a33585
cherry2501 <saicharangudiya@gmail.com>
2024-10-22 21:22:27 +0530

    backup

--- 19-mount.sh
+++ 19-mount.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-MOUNT=(df -hT | grep xfs | awk -F " " '{print $NF}')
-STORAGE=(df -hT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+MOUNT=$(df -hT | grep xfs | awk -F " " '{print $NF}')
+STORAGE=$(df -hT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
 
 echo "$MOUNT"
 echo "$STORAGE"
