a02fb81a86c813bd9a847a23efacacec68630f45
cherry2501 <saicharangudiya@gmail.com>
2024-10-22 21:45:25 +0530

    mount

--- 19-mount.sh
+++ 19-mount.sh
@@ -5,7 +5,7 @@ DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5
 #STORAGE=$(df -hT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
 
-while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+while IFS= read -r line #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
 do
     USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $line | grep xfs | | awk -F " " '{print $NF}')
