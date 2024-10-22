eb4284dda74f360b94bd7f3331cd0c6f15537555
cherry2501 <saicharangudiya@gmail.com>
2024-10-22 19:06:42 +0530

    backup

--- 18-backup.sh
+++ 18-backup.sh
@@ -3,6 +3,8 @@
 SOURCE_DIR=$1
 DESTINATION_DIR=$2
 DAYS=${3:-14}
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
@@ -33,6 +35,24 @@ echo "Files: $FILES"
 if [ ! -z $FILES ] #true if FILES is empty, ! nakes it expression false
 then
     echo "Files are found"
+    ZIP_FILE="$DESTINATION_DIR/app-logs-$TIMESTAMP.zip"
+    find ${SOURCE_DIR} -name "*.log" -mtime +$DAYS | zip "$ZIP_FILE" -@
+
+    # check if zip is created or not
+
+    if [ -f $ZIP_FILE ]
+    then
+        echo "successfully zipped files older than $DAYS..."
+
+        #remove files after zipping
+        while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+        do
+            echo "Deleting file: $file"
+            rm -rf $file
+        done <<< $FILES
+    else
+        echo "Failure in zipping files.."
+        exit 1
 else
     echo "No Files older than $DAYS days"
 fi
