4d3172f011cd218c2424692521a4cdf0d653acf1
cherry2501 <saicharangudiya@gmail.com>
2024-10-22 18:36:35 +0530

    backup

--- 18-backup.sh
+++ 18-backup.sh
@@ -28,11 +28,11 @@ then
 fi
 
 FILES=$(find $(SOURCE_DIR) -name ".*log" -mtime +$DAYS)
-echo "files: $FILES"
+echo "Files: $FILES"
 
 if [ ! -z $FILES ] #true if FILES is empty, ! nakes it expression false
 then
     echo "Files are found"
 else
-    echo "No Files older than $DAYS"
+    echo "No Files older than $DAYS days"
 fi
