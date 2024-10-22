fbf1dabda7e99b2aefdc9cb13bff20dc5ef2e878
cherry2501 <saicharangudiya@gmail.com>
2024-10-22 21:42:53 +0530

    backup

--- 19-mount.sh
+++ 19-mount.sh
@@ -7,9 +7,8 @@ DISK_THRESHOLD=5
 
 while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
 do
-    echo "Deleting file: $line"
-    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    PARTITION=$(echo $line | awk -F " " '{print $NF}')
+    USAGE=$(echo $line | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | grep xfs | | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD ]
     then 
         echo "$PARTITION is more than $DISK_THRESHOLD, current value:$USAGE... check it"
