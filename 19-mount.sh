263c2a4bd454aef67d366b9bb00ed63828339284
cherry2501 <saicharangudiya@gmail.com>
2024-10-22 21:38:37 +0530

    backup

--- 19-mount.sh
+++ 19-mount.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-MOUNT=$(df -hT | grep xfs | awk -F " " '{print $NF}')
-STORAGE=$(df -hT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
+DISK_USAGE=$(df -hT | grep xfs)
+#MOUNT=$(df -hT | grep xfs | awk -F " " '{print $NF}')
+DISK_THRESHOLD=5
+#STORAGE=$(df -hT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1)
 
-echo "$MOUNT"
-echo "$STORAGE"
+while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+do
+    echo "Deleting file: $line"
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk -F " " '{print $NF}')
+    if [ $USAGE -gt $DISK_THRESHOLD ]
+    then 
+        echo "$PARTITION is more than $DISK_THRESHOLD, current value:$USAGE... check it"
+    fi
+done <<< $DISK_USAGE
+
